e023b579a306d9318efafbd9b58d26d4e4bc00c5
Daniel Saidi <daniel.saidi@gmail.com>
2021-03-31 22:37:15 +0200

    Add macOS target

---
+++ trySwiftColorKitPro-macOS/trySwiftColorKitPro_macOS.h
@@ -0,0 +1,18 @@
+//
+//  trySwiftColorKitPro_macOS.h
+//  trySwiftColorKitPro-macOS
+//
+//  Created by Daniel Saidi on 2021-03-31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for trySwiftColorKitPro_macOS.
+FOUNDATION_EXPORT double trySwiftColorKitPro_macOSVersionNumber;
+
+//! Project version string for trySwiftColorKitPro_macOS.
+FOUNDATION_EXPORT const unsigned char trySwiftColorKitPro_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <trySwiftColorKitPro_macOS/PublicHeader.h>
+
+
