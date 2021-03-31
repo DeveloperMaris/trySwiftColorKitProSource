3bd723b193a346addd74f7e3478e62d895f5e40f
Daniel Saidi <daniel.saidi@gmail.com>
2021-03-31 21:11:20 +0200

    Create new Framework project

---
+++ trySwiftColorKitPro/trySwiftColorKitPro.h
@@ -0,0 +1,18 @@
+//
+//  trySwiftColorKitPro.h
+//  trySwiftColorKitPro
+//
+//  Created by Daniel Saidi on 2021-03-31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for trySwiftColorKitPro.
+FOUNDATION_EXPORT double trySwiftColorKitProVersionNumber;
+
+//! Project version string for trySwiftColorKitPro.
+FOUNDATION_EXPORT const unsigned char trySwiftColorKitProVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <trySwiftColorKitPro/PublicHeader.h>
+
+
