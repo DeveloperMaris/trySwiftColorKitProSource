7c544ef7faf0fc1f6a9913a699ccaa9e54a7208f
Daniel Saidi <daniel.saidi@gmail.com>
2021-03-31 21:23:41 +0200

    Add Color extension with PRO(ish) colors

---
+++ trySwiftColorKitPro/Color+Pro.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Pro.swift
+//  trySwiftColorKitPro
+//
+//  Created by Daniel Saidi on 2021-03-31.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    static var proBlue: Color { .blue }
+    static var proGreen: Color { .green }
+    static var proRed: Color { .red }
+    static var proYellow: Color { .green }
+}
