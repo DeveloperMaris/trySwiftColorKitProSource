d649d643fdc8c001a9d683d12a55b0b346a113fb
Daniel Saidi <daniel.saidi@gmail.com>
2021-04-20 19:56:24 +0200

    Add framework

---
+++ trySwiftColorKitPro/trySwiftColorKitPro/Color+Pro.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Pro.swift
+//  trySwiftColorKitPro
+//
+//  Created by Daniel Saidi on 2021-04-20.
+//
+
+import SwiftUI
+import trySwiftColorKit
+
+public extension Color {
+    
+    static var proBlue: Color { .deepBlue }
+    static var proGreen: Color { .deepGreen }
+    static var proRed: Color { .deepRed }
+    static var proYellow: Color { .deepYellow }
+}
